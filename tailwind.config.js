e9e87527fd91c25f060b0c8885043bf88be89804
Kiran PS <pskirann@gmail.com>
2019-06-26 19:29:16 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ghostwhite: "#f8f8ff"
+      }
+    }
+  },
+  variants: {
+    outline: ["focus", "responsive", "hover"]
+  },
+  plugins: []
+};
